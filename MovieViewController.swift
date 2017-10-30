8f6f959e0addce48e3faa7a3819097e831f92f3b
Chuck Konkol <ckonkol@gmail.com>
2017-10-30 18:57:33 -0500

    ck

---
+++ MovieViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MovieViewController.swift
+//  MyMovies
+//
+//  Created by cis290 on 10/30/17.
+//  Copyright © 2017 Rock Valley College. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class MovieViewController: AVPlayerViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .landscapeLeft
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
