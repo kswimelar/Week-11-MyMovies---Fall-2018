ec0dc1b70a7b09f751ffffd05378191e4690b140
kswimelar <35944507+kswimelar@users.noreply.github.com>
2018-10-29 17:52:04 -0500

    ks code_added

--- MovieViewController.swift
+++ MovieViewController.swift
@@ -35,5 +35,76 @@ class MovieViewController: AVPlayerViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        // Get the new view controller using segue.destinationViewController.
+        
+        // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "first"
+            
+        {
+            
+            let destination = segue.destination as!
+                
+            MovieViewController
+            
+            //Change to match you video filename in supporting files
+            
+            let termsPath:String? = Bundle.main.path(forResource: "trailer1", ofType: "mp4")!
+            
+            let url = NSURL(fileURLWithPath: termsPath!)
+            
+            
+            
+            destination.player = AVPlayer(url: url as URL)
+            
+            destination.player!.play()
+            
+        }
+        
+        if segue.identifier == "second"
+            
+        {
+            
+            let destination = segue.destination as!
+                
+            MovieViewController
+            
+            //Change to match you video filename in supporting files
+            
+            let termsPath:String? = Bundle.main.path(forResource: "trailer2", ofType: "mp4")!
+            
+            let url = NSURL(fileURLWithPath: termsPath!)
+            
+            destination.player = AVPlayer(url: url as URL)
+            
+            destination.player!.play()
+            
+        }
+        
+        if segue.identifier == "third"
+            
+        {
+            
+            let destination = segue.destination as!
+                
+            MovieViewController
+            
+            //Change to match you video filename in supporting files
+            
+            let termsPath:String? = Bundle.main.path(forResource: "trailer3", ofType: "mp4")!
+            
+            let url = NSURL(fileURLWithPath: termsPath!)
+            
+            destination.player = AVPlayer(url: url as URL)
+            
+            destination.player!.play()
+            
+        }
+        
+        
+        
+    }
 
 }
